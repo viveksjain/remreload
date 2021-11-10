4911b2be59c5795f02e6cf60ab4c5c9a88501a74
Vivek Jain <vivek.jain@rubrik.com>
2021-11-09 19:12:28 -0800

    It works!

--- src/extension.ts
+++ src/extension.ts
@@ -3,29 +3,48 @@
 import * as vscode from 'vscode';
 import {spawn} from 'child_process';
 
+let DEBUG = false;
+let TIMEOUT = DEBUG ? 5 : 3600;
 let output: vscode.OutputChannel | null = null;
+let shouldTrigger = false;
+let interval: NodeJS.Timeout | null = null;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	output = vscode.window.createOutputChannel("Autoreload");
-	if (vscode.env.remoteName === 'ssh-remote') {
-		output.appendLine('HERE');
+	if (vscode.env.remoteName === 'ssh-remote' || DEBUG) {
+		output.appendLine('Starting');
 		let lastTimer = Date.now();
-		setInterval(() => {
+		interval = setInterval(() => {
+			if (shouldTrigger) {
+				let ping = spawn('ping', ['-c', '1', 'vivek-jain-l01.colo.rubrik.com']);
+				ping.on('close', (code) => {
+					if (code === 0) {
+						vscode.commands.executeCommand('workbench.action.reloadWindow');
+					}
+					output!.appendLine(`ping exited with code ${code}`);
+				});
+				return;
+			}
+
 			var now = Date.now();
 			var diff = (now - lastTimer) / 1000;
 			output!.appendLine(`${diff} secs since last update`);
-			if(diff > 3600) {
-				output!.appendLine('Forcing reload');
-				vscode.commands.executeCommand('workbench.action.reloadWindow');
+			if(diff > TIMEOUT) {
+				output!.appendLine('Timeout hit!');
+				shouldTrigger = true;
 			}
 			lastTimer = now;
-		}, 5000);
+		}, 2000);
 	} else if (vscode.env.remoteName) {
 		output.appendLine(vscode.env.remoteName!);
 	}
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+	if (interval) {
+		clearInterval(interval);
+	}
+}
